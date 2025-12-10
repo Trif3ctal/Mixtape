a74b844c0f6528359c7e2e2a0dc72a546a40bf61
the-Astra <djs8743@rit.edu>
2025-12-09 20:41:32 -0500

    Prevent multiple add sounds from playing at the same time

--- src/utils.lua
+++ src/utils.lua
@@ -1,6 +1,13 @@
+--#region States
+BalatroGoesGold.STATES = {
+    add_sound_playing = false
+}
+--#endregion
+
 --#region Sound on add
 BalatroGoesGold.calculate = function(self, context)
-    if context.card_added and context.card.config.center.bgg_addsound and BalatroGoesGold.config.add_sounds then
+    if context.card_added and context.card.config.center.bgg_addsound and BalatroGoesGold.config.add_sounds and not BalatroGoesGold.STATES.add_sound_playing then
+        BalatroGoesGold.STATES.add_sound_playing = true
         local sound = context.card.config.center.bgg_addsound
         if type(sound) == 'table' then
             sound = pseudorandom_element(sound)
@@ -21,6 +28,7 @@ BalatroGoesGold.calculate = function(self, context)
             trigger = 'after',
             func = function()
                 if G.SETTINGS.SOUND.music_volume >= og_volume then
+                    BalatroGoesGold.STATES.add_sound_playing = false
                     G.SETTINGS.SOUND.music_volume = og_volume
                     G:save_settings()
                     return true
@@ -38,7 +46,8 @@ end
 local cc = Card.click
 function Card:click()
     cc(self)
-    if self.area and self.area.config.collection and self.config.center.bgg_addsound and BalatroGoesGold.config.add_sounds then
+    if self.area and self.area.config.collection and self.config.center.bgg_addsound and BalatroGoesGold.config.add_sounds and not BalatroGoesGold.STATES.add_sound_playing then
+        BalatroGoesGold.STATES.add_sound_playing = true
         local sound = self.config.center.bgg_addsound
         if type(sound) == 'table' then
             sound = pseudorandom_element(sound)
@@ -57,6 +66,7 @@ function Card:click()
             trigger = 'after',
             func = function()
                 if G.SETTINGS.SOUND.music_volume >= og_volume then
+                    BalatroGoesGold.STATES.add_sound_playing = false
                     G.SETTINGS.SOUND.music_volume = og_volume
                     G:save_settings()
                     return true
