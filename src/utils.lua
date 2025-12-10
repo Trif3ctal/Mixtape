6231f49f310fd7a62c7ce060fff97407c7435f56
the-Astra <djs8743@rit.edu>
2025-12-09 20:15:25 -0500

    Add support for multiple sound keys

--- src/utils.lua
+++ src/utils.lua
@@ -1,18 +1,21 @@
 --#region Sound on add
 BalatroGoesGold.calculate = function(self, context)
     if context.card_added and context.card.config.center.bgg_addsound and BalatroGoesGold.config.add_sounds then
+        local sound = context.card.config.center.bgg_addsound
+        if type(sound) == 'table' then
+            sound = pseudorandom_element(sound)
+        end
         G.E_MANAGER:add_event(Event({
             delay = 0.4,
             trigger = 'after',
             func = function()
                 context.card:juice_up()
-                play_sound(context.card.config.center.bgg_addsound)
+                play_sound(sound)
                 return true;
             end
         }))
     end
 end
-
 --#endregion
 
 --#region Album ObjectType
