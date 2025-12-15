9fa248230fe44b81cb5f397b4d7fb5da558ee1ef
the-Astra <djs8743@rit.edu>
2025-12-14 23:23:52 -0500

    jonkers

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -2,107 +2,107 @@ return {
     descriptions = {
         Joker = {
             j_bgg_RAM = {
-                name = {'Random Access Memories', '{s:0.8,C:inactive}Daft Punk'},
+                name = { 'Random Access Memories', '{s:0.8,C:inactive}Daft Punk' },
                 text = {
 
                 }
             },
             j_bgg_Chaser = {
-                name = {'CHASER', '{s:0.8,C:inactive}Femtanyl'},
+                name = { 'CHASER', '{s:0.8,C:inactive}Femtanyl' },
                 text = {
 
                 }
             },
             j_bgg_Chromakopia = {
-                name = {'CHROMAKOPIA', '{s:0.8,C:inactive}Tyler, the Creator'},
+                name = { 'CHROMAKOPIA', '{s:0.8,C:inactive}Tyler, the Creator' },
                 text = {
 
                 }
             },
             j_bgg_Worlds = {
-                name = {'Worlds', '{s:0.8,C:inactive}Porter Robinson'},
+                name = { 'Worlds', '{s:0.8,C:inactive}Porter Robinson' },
                 text = {
 
                 }
             },
             j_bgg_Cross = {
-                name = {'{f:bgg_emoji}†', '{s:0.8,C:inactive}Justice'},
-                text = {{
+                name = { '{f:bgg_emoji}†', '{s:0.8,C:inactive}Justice' },
+                text = { {
                     "Permanently {C:red}debuffs{} the",
                     "{C:attention}rightmost{} scoring card"
-                },{
+                }, {
                     "{X:mult,C:white}X#1#{} Mult for each {C:red}debuffed",
                     "card in scoring hand"
-                }}
+                } }
             },
             j_bgg_RunTheJewels3 = {
-                name = {'Run The Jewels 3', '{s:0.8,C:inactive}Run The Jewels'},
+                name = { 'Run The Jewels 3', '{s:0.8,C:inactive}Run The Jewels' },
                 text = {
 
                 }
             },
             j_bgg_BitterTongues = {
-                name = {'Bitter Tongues', '{s:0.8,C:inactive}James Mariott'},
+                name = { 'Bitter Tongues', '{s:0.8,C:inactive}James Mariott' },
                 text = {
 
                 }
             },
             j_bgg_DarkSide = {
-                name = {'The Dark Side', 'Of The Moon','{s:0.8,C:inactive}Pink Floyd'},
+                name = { 'The Dark Side', 'Of The Moon', '{s:0.8,C:inactive}Pink Floyd' },
                 text = {
 
                 }
             },
             j_bgg_Rumours = {
-                name = {'Rumours', '{s:0.8,C:inactive}Fleetwood Mac'},
+                name = { 'Rumours', '{s:0.8,C:inactive}Fleetwood Mac' },
                 text = {
 
                 }
             },
             j_bgg_BlueAlbum = {
-                name = {'The Blue Album', '{s:0.8,C:inactive}Weezer'},
+                name = { 'The Blue Album', '{s:0.8,C:inactive}Weezer' },
                 text = {
 
                 }
             },
             j_bgg_MMFOOD = {
-                name = {'MM..FOOD', '{s:0.8,C:inactive}MF DOOM'},
+                name = { 'MM..FOOD', '{s:0.8,C:inactive}MF DOOM' },
                 text = {
 
                 }
             },
             j_bgg_SunkCostFallacy = {
-                name = {'Sunk Cost Fallacy', '{s:0.8,C:inactive}Fox Stevenson'},
+                name = { 'Sunk Cost Fallacy', '{s:0.8,C:inactive}Fox Stevenson' },
                 text = {
 
                 }
             },
             j_bgg_Vessels = {
-                name = {'Vessels', '{s:0.8,C:inactive}Starset'},
+                name = { 'Vessels', '{s:0.8,C:inactive}Starset' },
                 text = {
 
                 }
             },
             j_bgg_DemonDays = {
-                name = {'Demon Days', '{s:0.8,C:inactive}Gorillaz'},
+                name = { 'Demon Days', '{s:0.8,C:inactive}Gorillaz' },
                 text = {
 
                 }
             },
             j_bgg_SkinnyFists = {
-                name = {'Lift Your Skinny Fists', 'like Antennas to Heaven', '{s:0.8,C:inactive}Godspeed You Black Emperor!'},
+                name = { 'Lift Your Skinny Fists', 'like Antennas to Heaven', '{s:0.8,C:inactive}Godspeed You Black Emperor!' },
                 text = {
 
                 }
             },
             j_bgg_Bends = {
-                name = {'The Bends', '{s:0.8,C:inactive}Radiohead'},
+                name = { 'The Bends', '{s:0.8,C:inactive}Radiohead' },
                 text = {
 
                 }
             },
             j_bgg_VelvetUnderground = {
-                name = {'The Velvet', 'Underground & Nico', '{s:0.8,C:inactive}The Velvet Underground & Nico'},
+                name = { 'The Velvet', 'Underground & Nico', '{s:0.8,C:inactive}The Velvet Underground & Nico' },
                 text = {
                     '{C:chips}+#1#{} Chips', -- mimicking vanilla gros michel
                     '{C:green}#2# in #3#{} chance this',
@@ -111,7 +111,7 @@ return {
                 }
             },
             j_bgg_Blackstar = {
-                name = {'{f:bgg_emoji}★', '{s:0.8,C:inactive}David Bowie'},
+                name = { '{f:bgg_emoji}★', '{s:0.8,C:inactive}David Bowie' },
                 text = {
                     '{C:mult}+#1#{} Mult if played',
                     'hand contains a {C:diamonds}Diamond{}',
@@ -119,31 +119,31 @@ return {
                 }
             },
             j_bgg_LONGSEASON = {
-                name = {'LONG SEASON', '{s:0.8,C:inactive}Fishmans'},
+                name = { 'LONG SEASON', '{s:0.8,C:inactive}Fishmans' },
                 text = {
 
                 }
             },
             j_bgg_Aleph = {
-                name = {'Aleph', '{s:0.8,C:inactive}Gesaffelstein'},
+                name = { 'Aleph', '{s:0.8,C:inactive}Gesaffelstein' },
                 text = {
 
                 }
             },
             j_bgg_WhatsGoingOn = {
-                name = {'What\'s Going On', '{s:0.8,C:inactive}Marvin Gaye'},
+                name = { 'What\'s Going On', '{s:0.8,C:inactive}Marvin Gaye' },
                 text = {
 
                 }
             },
             j_bgg_Vignette = {
-                name = {'Vignette', '{s:0.8,C:inactive}Frums'},
+                name = { 'Vignette', '{s:0.8,C:inactive}Frums' },
                 text = {
 
                 }
             },
             j_bgg_Vanisher = {
-                name = {'Vanisher,', 'Horizon Scraper', '{s:0.8,C:inactive}Quadeca'},
+                name = { 'Vanisher,', 'Horizon Scraper', '{s:0.8,C:inactive}Quadeca' },
                 text = {
                     'Gains {X:mult,C:white}X#1#{} Mult when',
                     '{C:attention}Ante{} increases',
@@ -151,7 +151,7 @@ return {
                 }
             },
             j_bgg_GlowPt2 = {
-                name = {'The Glow, Pt. 2', '{s:0.8,C:inactive}The Microphones'},
+                name = { 'The Glow, Pt. 2', '{s:0.8,C:inactive}The Microphones' },
                 text = {
                     'Gains {C:mult}+#1#{} Mult per hand',
                     'played, {C:attention}resets{} if score',
@@ -160,37 +160,37 @@ return {
                 }
             },
             j_bgg_NeverKnowWhy = {
-                name = {'You Will Never Know Why', '{s:0.8,C:inactive}Sweet Trip'},
+                name = { 'You Will Never Know Why', '{s:0.8,C:inactive}Sweet Trip' },
                 text = {
 
                 }
             },
             j_bgg_WhitePony = {
-                name = {'White Pony', '{s:0.8,C:inactive}Deftones'},
+                name = { 'White Pony', '{s:0.8,C:inactive}Deftones' },
                 text = {
 
                 }
             },
             j_bgg_NothingButBest = {
-                name = {'Nothing But The Best', '{s:0.8,C:inactive}Frank Sinatra'},
+                name = { 'Nothing But The Best', '{s:0.8,C:inactive}Frank Sinatra' },
                 text = {
 
                 }
             },
             j_bgg_Very1999Christmas = {
-                name = {'A Very 1999 Christmas', '{s:0.8,C:inactive}Jschlatt'},
+                name = { 'A Very 1999 Christmas', '{s:0.8,C:inactive}Jschlatt' },
                 text = {
 
                 }
             },
             j_bgg_CrimsonKing = {
-                name = {'In The Court of the', 'Crimson King', '{s:0.8,C:inactive}King Crimson'},
+                name = { 'In The Court of the', 'Crimson King', '{s:0.8,C:inactive}King Crimson' },
                 text = {
 
                 }
             },
             j_bgg_GunfighterBallads = {
-                name = {'Gunfighter Ballads', 'and Trail Songs', '{s:0.8,C:inactive}Marty Robbins'},
+                name = { 'Gunfighter Ballads', 'and Trail Songs', '{s:0.8,C:inactive}Marty Robbins' },
                 text = {
                     "During {C:attention}Boss{} Blinds,",
                     "if first played hand's rank value",
@@ -200,61 +200,79 @@ return {
                 }
             },
             j_bgg_TeenageDreams = {
-                name = {'Teenage Dreams', '{s:0.8,C:inactive}Katy Perry'},
+                name = { 'Teenage Dreams', '{s:0.8,C:inactive}Katy Perry' },
                 text = {
 
                 }
             },
             j_bgg_Fragile = {
-                name = {'Fragile', '{s:0.8,C:inactive}Yes'},
+                name = { 'Fragile', '{s:0.8,C:inactive}Yes' },
                 text = {
 
                 }
             },
             j_bgg_1989 = {
-                name = {'1989', '{s:0.8,C:inactive}Taylor Swift'},
+                name = { '1989', '{s:0.8,C:inactive}Taylor Swift' },
                 text = {
 
                 }
             },
             j_bgg_V = {
-                name = {'V', '{s:0.8,C:inactive}Maroon Five'},
+                name = { 'V', '{s:0.8,C:inactive}Maroon Five' },
                 text = {
 
                 }
             },
             j_bgg_Currents = {
-                name = {'Currents', '{s:0.8,C:inactive}Tame Impala'},
+                name = { 'Currents', '{s:0.8,C:inactive}Tame Impala' },
                 text = {
 
                 }
             },
             j_bgg_SashaFierce = {
-                name = {'I Am... Sasha Fierce', '{s:0.8,C:inactive}Beyoncé'},
+                name = { 'I Am... Sasha Fierce', '{s:0.8,C:inactive}Beyoncé' },
                 text = {
 
                 }
             },
             j_bgg_brat = {
-                name = {'brat', '{s:0.8,C:inactive}Charli xcx'},
+                name = { 'brat', '{s:0.8,C:inactive}Charli xcx' },
                 text = {
 
                 }
             },
             j_bgg_444 = {
-                name = {'4:44', '{s:0.8,C:inactive}JAY-Z'},
+                name = { '4:44', '{s:0.8,C:inactive}JAY-Z' },
                 text = {
 
                 }
             },
             j_bgg_TravellingWithoutMoving = {
-                name = {'Travelling Without Moving', '{s:0.8,C:inactive}Jamiroquai'},
+                name = { 'Travelling Without Moving', '{s:0.8,C:inactive}Jamiroquai' },
                 text = {
                     'After {C:money}purchasing{} a card',
                     'from the shop, move all shop cards',
                     'to the left and {C:attention}restock{} one shop card'
                 }
-            }
+            },
+            j_bgg_AM = {
+                name = { 'AM [2013]', '{s:0.8,C:inactive}Arctic Monkeys' },
+                text = {
+
+                }
+            },
+            j_bgg_ChannelOrange = {
+                name = { 'channel ORANGE [2012]', '{s:0.8,C:inactive}Frank Ocean' },
+                text = {
+
+                }
+            },
+            j_bgg_2001 = {
+                name = { '2001 [1999]', '{s:0.8,C:inactive}Dr. Dre' },
+                text = {
+
+                }
+            },
         },
         Other = {
             p_bgg_mashup_jumbo_1 = {
